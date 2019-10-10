5117e5da6eef1f33e5101fb98cd0b038ac286d26
Jahanzaib <101974245@student.swin.edu.au>
2019-10-10 14:19:31 +1100

    Background change, AIEasyBot

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -35,6 +35,7 @@ static class MenuController
 			"QUIT"
 		},
 		new string[] {
+			"EASY"
 			"MEDIUM",
 			"HARD"
 		}
@@ -295,7 +296,9 @@ static class MenuController
 	private static void PerformSetupMenuAction(int button)
 	{
 		switch (button) {
-			
+			case SETUP_MENU_EASY_BUTTON:
+				GameController.SetDifficulty(AIOption.Easy);
+				break;
 			case SETUP_MENU_MEDIUM_BUTTON:
 				GameController.SetDifficulty(AIOption.Medium);
 				break;
